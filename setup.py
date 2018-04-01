e902ebfe1fabb28df8b2f7bde55306d81e7925d7
Davide Moro <dmoro@tierratelematics.com>
2018-04-01 23:18:29 +0200

    Preparing release 2.0.2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ docs_require = [
 
 setup(
     name='pytest-pypom-navigation',
-    version='2.0.2.dev0',
+    version='2.0.2',
     author='Davide Moro',
     author_email='davide.moro@gmail.com',
     maintainer='Davide Moro',

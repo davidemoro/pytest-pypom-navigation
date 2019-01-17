a9aa4ea83f24bc7fd309a3e0b8b3d2e217053794
Davide Moro <davide.moro@gmail.com>
2019-01-17 22:43:52 +0100

    Back to development: 2.0.4

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ docs_require = [
 
 setup(
     name='pytest-pypom-navigation',
-    version='2.0.3',
+    version='2.0.4.dev0',
     author='Davide Moro',
     author_email='davide.moro@gmail.com',
     maintainer='Davide Moro',
